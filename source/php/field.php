ff7851334a9e6644e43a5047bbbd02a64a0384c2
NiclasNorin <niclasn@outlook.com>
2025-09-01 08:51:10 +0200

    refactor: css classes

--- source/php/field.php
+++ source/php/field.php
@@ -28,7 +28,6 @@ class Field extends \acf_field
 
         ?>
         <div class="acf-icon-field__container" id="<?php echo $id; ?>" data-js-acf-icon-field="container">
-            <div data-js-acf-icon-field="preview" class="acf-icon-field__selected-preview">
             <div data-js-acf-icon-field="preview" class="acf-icon-field__selected-preview-container">
                 <div role="button" class="acf-icon-field__preview-clear" type="button" data-js-acf-icon-field="clear-button" aria-label="<?php __('Remove Icon', 'acf-icon-field'); ?>">
                     <span data-material-symbol="delete" class="acf-icon-field__preview-clear-icon material-symbols material-symbols-rounded material-symbols-sharp material-symbols-outlined">delete</span>
